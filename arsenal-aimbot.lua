37002de9298b88bfe801eadb9a9ee91d30f5705f
Kermutz <108731023+Kermutz@users.noreply.github.com>
2022-07-06 14:34:13 +0300

    Update arsenal-aimbot.lua

--- arsenal-aimbot.lua
+++ arsenal-aimbot.lua
@@ -1,50 +1,56 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("NORTHERN ARSENAL", "DarkTheme")
-local Tab = Window:NewTab("AIMBOT")
+local Window = Library.CreateLib("NORTHERN ARSENAL", "Synapse")
+local Main = Window:NewTab("Main")
+local RAGE = Main:NewSection("RAGE")
+RAGE:NewButton("ButtonText", "ButtonInfo", function()
+    print("Aimbot enabled")
+    local CurrentCamera = workspace.CurrentCamera
+    local Players = game.GetService(game, "Players")
+    local LocalPlayer = Players.LocalPlayer
+    local Mouse = LocalPlayer:GetMouse()
+    function ClosestPlayer()
+        local MaxDist, Closest = math.huge
+        for I, V in pairs(Players.GetPlayers(Players)) do
+            if V == LocalPlayer then continue end
+            if V.Team == LocalPlayer then continue end
+            if not V.Character then continue end
+            local Head = V.Character.FindFirstChild(V.Character, "Head")
+            if not Head then continue end
+            local Pos, Vis = CurrentCamera.WorldToScreenPoint(CurrentCamera, Head.Position)
+            if not Vis then continue end
+            local MousePos, TheirPos = Vector2.new(Mouse.X, Mouse.Y), Vector2.new(Pos.X, Pos.Y)
+            local Dist = (TheirPos - MousePos).Magnitude
+            if Dist < MaxDist then
+                MaxDist = Dist
+                Closest = V
+                print("working")
+            end
+        end
+        return Closest
+    end
 
-local CurrentCamera = workspace.CurrentCamera
-local Players = game.GetService(game, "Players")
-local LocalPlayer = Players.LocalPlayer
-local Mouse = LocalPlayer:GetMouse()
-function ClosestPlayer()
-    local MaxDist, Closest = math.huge
-    for I,V in pairs(Players.GetPlayers(Players)) do
-        if V == LocalPlayer then continue end
-        if V.Team == LocalPlayer then continue end
-        if not V.Character then continue end
-        local Head = V.Character.FindFirstChild(V.Character, "Head")
-        if not Head then continue end
-        local Pos, Vis = CurrentCamera.WorldToScreenPoint(CurrentCamera, Head.Position)
-        if not Vis then continue end
-        local MousePos, TheirPos = Vector2.new(Mouse.X, Mouse.Y), Vector2.new(Pos.X, Pos.Y)
-        local Dist = (TheirPos - MousePos).Magnitude
-        if Dist < MaxDist then
-            MaxDist = Dist
-            Closest = V
-            print("working")
+    local MT = getrawmetatable(game)
+    local OldNC = MT.__namecall
+    local OldIDX = MT.__index
+    setreadonly(MT, false)
+    MT.__namecall = newcclosure(function(self, ...)
+        local Args, Method = { ... }, getnamecallmethod()
+        if Method == "FindPartOnRayWithIgnoreList" and not checkcaller() then
+            local CP = ClosestPlayer()
+            if CP and CP.Character and CP.Character.FindFirstChild(CP.Character, "Head") then
+                Args[1] = Ray.new(CurrentCamera.CFrame.Position,
+                    (CP.Character.Head.Position - CurrentCamera.CFrame.Position).Unit * 1000)
+                return OldNC(self, unpack(Args))
+            end
         end
-    end
-    return Closest
-end
-local MT = getrawmetatable(game)
-local OldNC = MT.__namecall
-local OldIDX = MT.__index
-setreadonly(MT, false)
-MT.__namecall = newcclosure(function(self, ...)
-    local Args, Method = {...}, getnamecallmethod()
-    if Method == "FindPartOnRayWithIgnoreList" and not checkcaller() then
-        local CP = ClosestPlayer()
-        if CP and CP.Character and CP.Character.FindFirstChild(CP.Character, "Head") then
-            Args[1] = Ray.new(CurrentCamera.CFrame.Position, (CP.Character.Head.Position - CurrentCamera.CFrame.Position).Unit * 1000)
-            return OldNC(self, unpack(Args))
+        return OldNC(self, ...)
+    end)
+    MT.__index = newcclosure(function(self, K)
+        if K == "Clips" then
+            return workspace.Map
         end
-    end
-    return OldNC(self, ...)
+        return OldIDX(self, K)
+    end)
+    setreadonly(MT, true)
+
 end)
-MT.__index = newcclosure(function(self, K)
-    if K == "Clips" then
-        return workspace.Map
-    end
-    return OldIDX(self, K)
-end)
-setreadonly(MT, true)
